8552fe2ed0f4d876278c984d2506c1c6e22260d9
timmie20 <olutimilehinolayinka1234@gmail.com>
2024-10-21 16:19:39 +0100

    Fix nav header responsivness

--- src/components/common/home/Hero.jsx
+++ src/components/common/home/Hero.jsx
@@ -3,42 +3,44 @@ import { Link } from "react-router-dom";
 
 function Hero() {
   return (
-    <section className="mx-auto h-fit w-[90%]">
-      <div className="flex max-w-[380px] flex-col gap-3">
-        <h1 className="text wrap font-Poppins text-[40px] font-bold text-white">
-          Pass your Exams in Style !
-        </h1>
+    <>
+      <section className="mx-auto h-fit w-[90%]">
+        <div className="flex max-w-[380px] flex-col gap-3">
+          <h1 className="text wrap font-Poppins text-[40px] font-bold text-white">
+            Pass your Exams in Style !
+          </h1>
 
-        <div className="font-Inter inline-flex items-center text-xl font-semibold text-white">
-          <p className="border-r-4 border-white pr-3 leading-none">
-            Proper Preparation
-          </p>
-          <p className="ml-3 leading-none">Excellent Results</p>
+          <div className="font-Inter inline-flex items-center text-xl font-semibold text-white">
+            <p className="border-r-4 border-white pr-3 leading-none">
+              Proper Preparation
+            </p>
+            <p className="ml-3 leading-none">Excellent Results</p>
+          </div>
+
+          <Link
+            to="/sign-up"
+            className="font-Poppins my-6 h-fit w-fit rounded-[20px] bg-white px-9 py-4 text-xl font-semibold text-teal-700 no-underline transition-colors hover:bg-teal-100"
+          >
+            GET STARTED
+          </Link>
+
+          <div className="inline-flex items-center gap-3">
+            <CTAButtons
+              lightText="Available on the"
+              boldText="Google Play"
+              url="/Icons/playstore-icon.svg"
+              alt="Playstore button"
+            />
+            <CTAButtons
+              lightText="Download on the"
+              boldText="App Store"
+              url="/Icons/apple-icon.svg"
+              alt="Apple store button"
+            />
+          </div>
         </div>
-
-        <Link
-          to="/sign-up"
-          className="font-Poppins my-6 h-fit w-fit rounded-[20px] bg-white px-9 py-4 text-xl font-semibold text-teal-700 no-underline transition-colors hover:bg-teal-100"
-        >
-          GET STARTED
-        </Link>
-
-        <div className="inline-flex items-center gap-3">
-          <CTAButtons
-            lightText="Available on the"
-            boldText="Google Play"
-            url="/Icons/playstore-icon.svg"
-            alt="Playstore button"
-          />
-          <CTAButtons
-            lightText="Download on the"
-            boldText="App Store"
-            url="/Icons/apple-icon.svg"
-            alt="Apple store button"
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
